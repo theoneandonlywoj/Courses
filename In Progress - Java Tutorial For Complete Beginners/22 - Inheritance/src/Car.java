7162cc1212ee6801d129d5a90a9e8c809837d801
arcyfelix <wor@kth.se>
2017-10-08 20:09:09 +0100

    Adding 22 - Inheritance. Java Tutorial For Complete Beginners.

---
+++ In Progress - Java Tutorial For Complete Beginners/22 - Inheritance/src/Car.java
@@ -0,0 +1,10 @@
+
+public class Car extends Machine {
+	public void openDoor() {
+		System.out.println("Doors open!");
+	}
+
+	public void closeDoor() {
+		System.out.println("Doors closed!");
+	}
+}
