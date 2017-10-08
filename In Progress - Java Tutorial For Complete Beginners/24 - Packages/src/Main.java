610f129a0894d5ce00e4aba1e8adb7b0ec05ce0c
arcyfelix <wor@kth.se>
2017-10-08 21:23:39 +0100

    Adding 24 - Packages. Java Tutorial For Complete Beginners.

---
+++ In Progress - Java Tutorial For Complete Beginners/24 - Packages/src/Main.java
@@ -0,0 +1,16 @@
+// Single imports
+import ocean.Fish;
+import ocean.plants.Seaweed;
+// Importing all
+import ocean.*;
+
+// Or use CRTL + SHIFT + O for Eclipse
+
+public class Main {
+
+	public static void main(String[] args) {
+		Fish f = new Fish();
+		Seaweed s = new Seaweed();
+	}
+
+}
