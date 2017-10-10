14cef9865a91acd48bc2a86701caed3370b98607
arcyfelix <wor@kth.se>
2017-10-10 21:50:46 +0100

    Solving issues for project 21 and adding project 26 - Polymorphism. Java Tutorial For Complete Beginners

---
+++ In Progress - Java Tutorial For Complete Beginners/26 - Polymorphism/src/Plant.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Arcyfelix
+ */
+public class Plant {
+
+    public void grow() {
+        System.out.println("Plant growing!");
+    }
+
+}
