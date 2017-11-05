4a5a0443876a21db97ae74cec5d0950955e08517
arcyfelix <wor@kth.se>
2017-11-05 15:18:56 +0000

    Reorganizing the project folder for better visibility. Learning Apache Maven.

--- In Progress - Learning Apache Maven/02 - Maven examples/src/main/java/courses/arcyfelix/com/github/Application01.java
+++
@@ -1,8 +0,0 @@
-package courses.arcyfelix.com.github;
-
-public class Application01 {
-	public static void main(String[] args) {
-		System.out.println("Hi!");
-	}
-
-}
---
+++ In Progress - Learning Apache Maven/02 - Project Setup/src/main/java/com/github/arcyfelix/Main.java
@@ -0,0 +1,9 @@
+package com.github.arcyfelix;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Visit: https://github.com/arcyfelix/Courses");
+	}
+
+}
