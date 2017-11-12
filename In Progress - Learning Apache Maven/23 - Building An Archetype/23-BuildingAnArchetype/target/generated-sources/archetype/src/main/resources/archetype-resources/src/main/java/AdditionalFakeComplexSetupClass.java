d971417692b852fc4a27138c07b2c0233a926c3d
arcyfelix <wor@kth.se>
2017-11-12 23:26:25 +0000

    Adding project 23 - Building An Archetype. Learning Apache Maven

---
+++ In Progress - Learning Apache Maven/23 - Building An Archetype/23-BuildingAnArchetype/target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/AdditionalFakeComplexSetupClass.java
@@ -0,0 +1,10 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+public class AdditionalFakeComplexSetupClass {
+	public void printSomething() {
+		System.out.println("Some complicated setup here!");
+	}
+}
---
+++ In Progress - Learning Apache Maven/23 - Building An Archetype/23-BuildingAnArchetype/target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/AdditionalFakeComplexSetupClass.java
@@ -0,0 +1,7 @@
+package it.pkg;
+
+public class AdditionalFakeComplexSetupClass {
+	public void printSomething() {
+		System.out.println("Some complicated setup here!");
+	}
+}
