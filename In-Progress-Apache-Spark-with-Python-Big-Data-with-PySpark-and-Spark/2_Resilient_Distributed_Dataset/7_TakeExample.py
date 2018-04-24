a3eef108c326c92ed3897804cf95136024bafce9
Arcyfelix <wor@kth.se>
2018-04-24 07:57:08 +0100

    Adding example of using 'take' to peek at the data.

---
+++ In-Progress-Apache-Spark-with-Python-Big-Data-with-PySpark-and-Spark/2_Resilient_Distributed_Dataset/7_TakeExample.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("take").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
+    
+    inputWords = ["spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop"]
+    wordRdd = sc.parallelize(inputWords)
+    
+    # 'Take' returns first x number of elements
+    # Very useful for a brief peek at the dataset.
+    words = wordRdd.take(3)
+    for word in words:
+        print(word)
