de5142a0df2d40d40d12bd1c50f0012429377f44
arcyfelix <wor@kth.se>
2018-09-16 18:45:59 +0100

    Adding additional notes about drawbacks of extending the App trait.

--- In Progress - Programming in Scala by Martin Odersky, Lex Spoon and Bill Venners/07-TheAppTrait/src/main.scala
+++ In Progress - Programming in Scala by Martin Odersky, Lex Spoon and Bill Venners/07-TheAppTrait/src/main.scala
@@ -1,6 +1,12 @@
 /*
  * Scala Standalone Application.
  * Extending the "main" object with App trait.
+ * This solution has its shortcoming:
+ * 1) Command-line arguments are unavailable.
+ * 2) Multi-threating is unavailable because of JVM's limitations.
+ * 3) JVM is not optimising initialisation of the code.
+ * 
+ * This approach should be used only is the program is fairly simple.
  */
 object main extends App {
   for(season <- List("fall", "winter", "summer", "spring")){
