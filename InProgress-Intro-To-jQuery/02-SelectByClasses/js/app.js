1e77e62dfb7b2849c31ca2b127741152e6b2275c
arcyfelix <wor@kth.se>
2018-11-26 20:54:24 +0000

    Removing unwanted comments.

--- InProgress-Intro-To-jQuery/02-SelectByClasses/js/app.js
+++ InProgress-Intro-To-jQuery/02-SelectByClasses/js/app.js
@@ -1,7 +1,3 @@
-/*
-For this quiz, use a jQuery tag selector to grab all of the <li>s on the page!
-*/
-
 // Start with this variable! (don't delete it!)
 function logAllArticleItemClasses(){
             var articles;
