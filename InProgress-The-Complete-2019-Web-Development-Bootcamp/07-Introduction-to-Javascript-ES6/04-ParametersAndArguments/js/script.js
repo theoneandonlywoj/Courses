e110ce97c3096c19cbb9375a550d700a652872d2
arcyfelix <wor@kth.se>
2018-12-07 18:40:43 +0000

    Adding example of using a function with parameters.

---
+++ InProgress-The-Complete-2019-Web-Development-Bootcamp/07-Introduction-to-Javascript-ES6/04-ParametersAndArguments/js/script.js
@@ -0,0 +1,41 @@
+// Robot example
+function goToShop(){
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+}
+
+function comeBackHome(){
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+function getMilkNumberBottles(bottles) {
+  var cost = bottles * 1.5;
+
+  goToShop();
+  console.log("buy " + bottles + " bottle(s) of milk");
+  comeBackHome();
+}
+
+function getMilkWithMoney(money) {
+  var pricePerBottle = 1.5;
+  var bottlesBought = Math.floor(money  / pricePerBottle);
+
+  goToShop();
+  console.log("Bought " + bottlesBought + " bottle(s) of milk");
+  comeBackHome();
+}
