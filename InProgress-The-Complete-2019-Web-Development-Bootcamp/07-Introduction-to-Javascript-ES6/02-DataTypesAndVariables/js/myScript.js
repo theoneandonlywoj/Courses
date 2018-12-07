7656763f8f07f897d23cd6604efa34e39207ef2e
arcyfelix <wor@kth.se>
2018-12-07 09:24:31 +0000

    Adding code for data types and variables.

---
+++ InProgress-The-Complete-2019-Web-Development-Bootcamp/07-Introduction-to-Javascript-ES6/02-DataTypesAndVariables/js/myScript.js
@@ -0,0 +1,13 @@
+console.log(2 + 3);
+//
+console.log(typeof(23));
+console.log(typeof("Wojciech"));
+console.log(typeof(true));
+
+// Defining a variables
+var myName = "Wojciech";
+window.alert(myName);
+
+// Getting user feedback
+var yourName = prompt("What is your name?");
+window.alert("Hello, " + yourName + "!");
