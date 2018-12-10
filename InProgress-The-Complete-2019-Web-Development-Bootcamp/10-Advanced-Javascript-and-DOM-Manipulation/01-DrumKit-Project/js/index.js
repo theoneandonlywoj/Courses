3b11ec458b6a9ec252b34eca6b3811e065e8ff66
arcyfelix <wor@kth.se>
2018-12-10 19:25:37 +0000

    Adding an event listener to all buttons.

--- InProgress-The-Complete-2019-Web-Development-Bootcamp/10-Advanced-Javascript-and-DOM-Manipulation/01-DrumKit-Project/js/index.js
+++ InProgress-The-Complete-2019-Web-Development-Bootcamp/10-Advanced-Javascript-and-DOM-Manipulation/01-DrumKit-Project/js/index.js
@@ -1,10 +1,8 @@
 // Adding an Event Listener
 // The name of the function (without the "()") is passed to the event listener
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick(){
-  alert("I got clicked");
+// Adding the event listener to all button (with class drum)
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    alert("I just got clicked!");
+  });
 }
-document.querySelectorAll("button")[1].addEventListener("click", function(){
-  alert("I got clicked 2");
-});
