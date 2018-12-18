81eacdfe61fce8fec22ccdb2b691f87a536377e0
arcyfelix <wor@kth.se>
2018-12-18 20:48:42 +0000

    Adding POST method parsing the data from the sign up page.

--- InProgress-The-Complete-2019-Web-Development-Bootcamp/13-APIs/02-Mail-Chimp-Newsletter-Sign-Up-Page/app.js
+++ InProgress-The-Complete-2019-Web-Development-Bootcamp/13-APIs/02-Mail-Chimp-Newsletter-Sign-Up-Page/app.js
@@ -8,9 +8,18 @@ const port = 3000;
 
 // Adding reference to the public folder where the CSS files and the images are stored
 app.use(express.static("public"));
+// Using body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(port, ()=> {
