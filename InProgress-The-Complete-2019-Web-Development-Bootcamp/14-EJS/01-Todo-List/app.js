e5a05ae0e0de590a2e6f2da5ed1daa19044a1cd6
arcyfelix <wor@kth.se>
2018-12-21 11:03:30 +0000

    Changing date display format. Adding code for adding of multiple items to the list.

--- InProgress-The-Complete-2019-Web-Development-Bootcamp/14-EJS/01-Todo-List/app.js
+++ InProgress-The-Complete-2019-Web-Development-Bootcamp/14-EJS/01-Todo-List/app.js
@@ -4,38 +4,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-  var todayDayOfTheWeekNumber = new Date().getDay();
-  var day = "";
+var items = [];
 
-  switch (todayDayOfTheWeekNumber){
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
+app.get("/", function(req, res){
+  var dateOptions = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
-  res.render('list', {'day': day});
+  var day = new Date().toLocaleDateString("en-US", dateOptions);
+
+  res.render('list', {'day': day, 'newListItems': items});
 });
+
+app.post("/", (req, res) => {
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
