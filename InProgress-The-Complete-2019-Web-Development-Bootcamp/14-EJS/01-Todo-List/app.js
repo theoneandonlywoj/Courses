68114fb757f3527e8b03578a2fc77560f55cdcf0
arcyfelix <wor@kth.se>
2018-12-21 09:58:15 +0000

    Adding usage of scriplet tags.

--- InProgress-The-Complete-2019-Web-Development-Bootcamp/14-EJS/01-Todo-List/app.js
+++ InProgress-The-Complete-2019-Web-Development-Bootcamp/14-EJS/01-Todo-List/app.js
@@ -8,13 +8,31 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-  var todayDayOfTheWeekNumber = new Date();
+  var todayDayOfTheWeekNumber = new Date().getDay();
   var day = "";
 
-  if (todayDayOfTheWeekNumber === 6 | todayDayOfTheWeekNumber === 0){
-    day = "weekend";
-  } else {
-    day = "weekday";
+  switch (todayDayOfTheWeekNumber){
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
   }
   res.render('list', {'day': day});
 });
