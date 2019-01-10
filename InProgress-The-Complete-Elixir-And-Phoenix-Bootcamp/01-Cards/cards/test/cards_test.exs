dbcf88a9ae7ee6c99797253a6b1502f00827879c
arcyfelix <wor@kth.se>
2019-01-10 20:19:28 +0000

    Adding a case test.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/01-Cards/cards/test/cards_test.exs
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/01-Cards/cards/test/cards_test.exs
@@ -5,4 +5,10 @@ defmodule CardsTest do
   test "greets the world" do
     assert Cards.hello() == "hi there!"
   end
+
+  test "Create_deck makes 20 Cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length === 20
+  end
+
 end
