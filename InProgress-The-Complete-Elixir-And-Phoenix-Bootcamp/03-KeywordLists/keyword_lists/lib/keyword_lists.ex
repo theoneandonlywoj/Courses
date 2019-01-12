0e521f2c251e71435afb0880f4147487ec95c8cf
arcyfelix <wor@kth.se>
2019-01-12 10:58:27 +0000

    Adding different methods of creating keyword lists.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/03-KeywordLists/keyword_lists/lib/keyword_lists.ex
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/03-KeywordLists/keyword_lists/lib/keyword_lists.ex
@@ -1,17 +1,31 @@
 defmodule KeywordLists do
   @moduledoc """
   Documentation for KeywordLists.
+
   """
   @doc """
-  Creating a keyword list.
+  Creating a keyword list created with tuples syntax.
 
   ## Examples
 
-      iex> KeywordLists.create_keyword_list
+      iex> KeywordLists.create_keyword_list_with_tuples
       [primary: "red", secondary: "blue", last: "yellow"]
   """
-  def create_keyword_list do
+  def create_keyword_list_with_tuples do
     keyword_list = [{:primary, "red"}, {:secondary, "blue"}, {:last, "yellow"}]
+    keyword_list
+  end
+
+  @doc """
+  Creating a keyword list created with simple syntax.
+
+  ## Examples
+
+      iex> KeywordLists.create_keyword_list_simple_syntax
+      [primary: "red", secondary: "blue", last: "yellow"]
+  """
+  def create_keyword_list_simple_syntax do
+    keyword_list = [primary: "red", secondary: "blue", last: "yellow"]
     keyword_list
   end
 
@@ -20,11 +34,12 @@ defmodule KeywordLists do
 
   ## Examples
 
-      iex> my_kwl = KeywordLists.create_keyword_list
+      iex> my_kwl = KeywordLists.create_keyword_list_simple_syntax
       iex> KeywordLists.obtain_data_from_key(my_kwl, :last)
       "yellow"
   """
   def obtain_data_from_key(keyword_list, key) do
     keyword_list[key]
   end
+
 end
