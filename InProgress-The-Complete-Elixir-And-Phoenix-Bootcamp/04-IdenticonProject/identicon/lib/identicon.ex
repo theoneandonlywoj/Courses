0e5cb6756271da053637787d43fab559aff5ce8a
arcyfelix <wor@kth.se>
2019-01-12 18:33:37 +0000

    Using a struct.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/04-IdenticonProject/identicon/lib/identicon.ex
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/04-IdenticonProject/identicon/lib/identicon.ex
@@ -19,14 +19,16 @@ defmodule Identicon do
   ## Examples
 
       iex> Identicon.hash_input('my_string')
-      [61, 33, 43, 33, 250, 215, 190, 214, 60, 31, 181, 96, 198, 165, 197, 208]
+      %Identicon.Image{ hex: [61, 33, 43, 33, 250, 215, 190, 214, 60, 31, 181, 96, 198, 165, 197, 208] }
       iex> Identicon.hash_input('my_string')
-      [61, 33, 43, 33, 250, 215, 190, 214, 60, 31, 181, 96, 198, 165, 197, 208]
+      %Identicon.Image{ hex: [61, 33, 43, 33, 250, 215, 190, 214, 60, 31, 181, 96, 198, 165, 197, 208] }
 
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 
 end
