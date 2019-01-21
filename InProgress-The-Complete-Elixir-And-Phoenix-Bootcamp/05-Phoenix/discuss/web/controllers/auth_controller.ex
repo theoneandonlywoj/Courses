e230c0b40a7e941a606f65317f63838babdb4fa9
arcyfelix <wor@kth.se>
2019-01-21 21:38:33 +0000

    Adding users on authentication.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/controllers/auth_controller.ex
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/controllers/auth_controller.ex
@@ -2,11 +2,34 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
+  # Aliasing the User model
+  alias Discuss.User
+
   def callback(conn, params) do
-    IO.puts("++++++++++")
-    IO.inspect(conn.assigns)
-    IO.puts("++++++++++")
-    IO.inspect(params)
-    IO.puts("++++++++++")
+    # Pattern matching to extract ueberauth_auth
+    %{assigns: %{ ueberauth_auth: auth }} = conn
+    # Extracting user data
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: auth.provider
+    }
+    # Creating a new User changeset
+    changeset = User.changeset(%User{}, user_params)
+    insert_or_update_user(changeset)
+  end
+
+  # A private helper function
+  defp insert_or_update_user(changeset) do
+    # Filtering by email
+    # It can return a User or nil
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        # It will return {:ok, data} or {:error, error_message}
+        Repo.insert(changeset)
+      user ->
+        # It will return {:ok, data}
+        {:ok, user}
+    end
   end
 end
