27c84fa29bd9f95920b32dc429422f985da73acd
arcyfelix <wor@kth.se>
2019-01-27 14:18:23 +0000

    Adding link to each topic.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/controllers/topic_controller.ex
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/controllers/topic_controller.ex
@@ -71,6 +71,11 @@ defmodule Discuss.TopicController do
     render conn, "index.html", topics_list: topics
   end
 
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    render conn, "show.html", topic_html_variable: topic
+  end
+
   def edit(conn, %{"id" => topic_id}) do
     # Selecting with an id
     topic = Repo.get(Topic, topic_id)
