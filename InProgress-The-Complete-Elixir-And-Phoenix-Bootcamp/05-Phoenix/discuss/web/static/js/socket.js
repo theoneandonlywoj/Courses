d59c32c09d20323a99fcc7d37f86f6d7d8bd6480
arcyfelix <wor@kth.se>
2019-01-28 19:33:11 +0000

    Refactoring connecting to a channel based on topic ID.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/static/js/socket.js
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/static/js/socket.js
@@ -3,13 +3,11 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments: ${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+}
 
-document.querySelector('button').addEventListener('click', () => {
-  channel.push('comment: hello', { hi: 'there2!'})
-})
-
-export default socket
+window.createSocket = createSocket
