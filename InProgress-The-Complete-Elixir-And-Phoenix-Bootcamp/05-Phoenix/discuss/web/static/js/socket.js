e82c705285ec963894f6e46cb784f7460762e0b5
arcyfelix <wor@kth.se>
2019-01-30 08:23:25 +0000

    Rendering a list of comments.

--- InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/static/js/socket.js
+++ InProgress-The-Complete-Elixir-And-Phoenix-Bootcamp/05-Phoenix/discuss/web/static/js/socket.js
@@ -6,7 +6,10 @@ socket.connect()
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments: ${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => { 
+      console.log("Channel joined!");
+      renderComments(resp.comments); 
+    })
     .receive("error", resp => { console.log("Unable to join", resp) })
 
   document.querySelector("button").addEventListener('click', () => {
@@ -15,4 +18,15 @@ const createSocket = (topicId) => {
   });
 }
 
+function renderComments(comments){
+  // Creating HTML for each comment to be shown
+  const renderedComments = comments.map(comment => {
+    return `
+    <li class="collection-item">
+      ${comment.content}
+    </li>
+    `
+  });
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+};
 window.createSocket = createSocket
