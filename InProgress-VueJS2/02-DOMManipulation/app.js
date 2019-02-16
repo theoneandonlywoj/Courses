90a0639e6a9ad3b7849233ebb372d2f050279877
arcyfelix <wor@kth.se>
2019-02-16 11:43:47 +0000

    Re-rendering block and adding html code from a variable.

--- InProgress-VueJS2/02-DOMManipulation/app.js
+++ InProgress-VueJS2/02-DOMManipulation/app.js
@@ -2,10 +2,12 @@ new Vue({
 	el: '#app',
 	data: {
 		title: 'Hello World!',
-    link: 'http://google.com'
+    link: 'http://google.com',
+    finishedLink: '<a href="http://google.com">Google (finished link)</a>'
 	},
   methods: {
     sayHello: function (){
+      this.title = 'Hello';
       return this.title;
     }
   }
