92574da2c5250920c58afc930bdfe877925da9b9
arcyfelix <wor@kth.se>
2019-02-16 14:12:46 +0000

    Linting the code.

--- InProgress-VueJS2/03-Assignment-01/app.js
+++ InProgress-VueJS2/03-Assignment-01/app.js
@@ -1,13 +1,13 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            name: 'Wojciech',
-            age: 27,
-            image: 'http://sfwallpaper.com/images/nice-images-15.jpg'
-        },
-        methods: {
-        random: function() {
-          return Math.random();
-          }
-        }
-    });
+  el: '#exercise',
+  data: {
+    name: 'Wojciech',
+    age: 27,
+    image: 'http://sfwallpaper.com/images/nice-images-15.jpg'
+  },
+  methods: {
+    random: function() {
+      return Math.random();
+    }
+  }
+});
