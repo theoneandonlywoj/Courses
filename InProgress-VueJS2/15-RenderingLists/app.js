0d11d6eba4a4f9efea25d839d96c179594da10b0
arcyfelix <wor@kth.se>
2019-02-22 23:16:39 +0000

    v-for directive.

---
+++ InProgress-VueJS2/15-RenderingLists/app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    people: [
+      {
+        name: 'Wojciech',
+        age: 27,
+        color: 'blue'
+      },
+      {
+        name: 'Jessie',
+        age: 'unknown',
+        color: 'yellow'
+      }
+    ]
+  }
+})
