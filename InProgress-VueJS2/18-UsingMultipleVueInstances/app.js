5ef261027bc7da5a14f770855d4efc81b1530776
arcyfelix <wor@kth.se>
2019-02-25 23:03:56 +0000

    Adding an example of using multiple instances.

---
+++ InProgress-VueJS2/18-UsingMultipleVueInstances/app.js
@@ -0,0 +1,33 @@
+new Vue({
+  el: '#app1',
+  data: {
+    title: 'The VueJS Instance',
+    showParagraph: false
+  },
+  methods: {
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
+    },
+    updateTitle: function(title) {
+      this.title = title;
+    }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+new Vue({
+  el: '#app2',
+  data: {
+    title: "The second Vue Instance"
+  }
+});
