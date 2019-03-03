22ce83a6bcee6c8bf7f7e2f1d395eca7001652a8
arcyfelix <wor@kth.se>
2019-03-03 13:16:50 +0000

    Adding example of communication from the parent to the child element.

---
+++ InProgress-VueJS2/27-CommunicationBetweenComponents/src/components/UserDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ name }}</p>
+  </div>
+</template>
+
+<script>
+// Getting data from the parent component using properties
+export default {
+  // 'name' must match name used in the template
+  props: ['name']
+}
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
