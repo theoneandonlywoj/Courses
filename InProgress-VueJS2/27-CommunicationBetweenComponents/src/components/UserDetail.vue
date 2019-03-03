bca7c3c88be618a3f614e2c093ab23881e053bd7
arcyfelix <wor@kth.se>
2019-03-03 15:08:39 +0000

    Property validation.

--- InProgress-VueJS2/27-CommunicationBetweenComponents/src/components/UserDetail.vue
+++ InProgress-VueJS2/27-CommunicationBetweenComponents/src/components/UserDetail.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="component">
-    <h3>You may view the User Details here</h3>
-    <p>Many Details</p>
-    <p>User Name: {{ switchName() }}</p>
-  </div>
+<div class="component">
+  <h3>You may view the User Details here</h3>
+  <p>Many Details</p>
+  <p>User Name: {{ switchName() }}</p>
+</div>
 </template>
 
 <script>
 // Getting data from the parent component using properties
 export default {
   // 'name' must match name used in the template
-  props: ['name'],
+  props: {
+    // Adding validation by passing an array of allowed types
+    name: {
+      type: String,
+      required: true
+    }
+  },
   methods: {
     switchName() {
       return this.name.split("").reverse().join("");
