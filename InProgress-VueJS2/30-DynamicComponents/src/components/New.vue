931cbac6eb7c7efdeceebcf87804dfd56f6ac0e5
arcyfelix <wor@kth.se>
2019-03-05 22:43:28 +0000

    Adding simple features showing that a dynamic component is destroyed when changed to another dynamic component.

--- InProgress-VueJS2/30-DynamicComponents/src/components/New.vue
+++ InProgress-VueJS2/30-DynamicComponents/src/components/New.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <h3>New Quote</h3>
+    <button v-on:click="counter++">Increase</button>
+    <p>Counter: {{ counter }}</p>
   </div>
-
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
+  destroyed(){
+    console.log("Component New Destroyed!");
+  }
 }
 </script>
 
