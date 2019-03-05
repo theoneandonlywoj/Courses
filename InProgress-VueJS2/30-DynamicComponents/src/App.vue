6d2c74c25cf91bac28f0bf86407b49c767042d02
arcyfelix <wor@kth.se>
2019-03-05 22:02:01 +0000

    Adding example of creating a dynamic component.

---
+++ InProgress-VueJS2/30-DynamicComponents/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <button v-on:click="selectedComponent='app-quote'">Quote</button>
+              <button v-on:click="selectedComponent='app-author'">Author</button>
+              <button v-on:click="selectedComponent='app-new'">New</button>
+              <hr>
+              <p>{{ selectedComponent }}</p>
+              <!-- Dynamic component -->
+              <component v-bind:is="selectedComponent">
+                <p>Default</p>
+              </component>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
+    export default {
+      data: function () {
+        return {
+          quoteTitle: 'My Quote For The Day',
+          selectedComponent: 'app-quote'
+        }
+      },
+      components: {
+        appQuote: Quote,
+        appAuthor: Author,
+        appNew: New
+      }
+    }
+</script>
+
+<style>
+</style>
