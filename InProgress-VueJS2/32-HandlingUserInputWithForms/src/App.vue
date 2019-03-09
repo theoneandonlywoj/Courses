9c58276e5d2d4664c62051ea129cb1bd1666ebe0
arcyfelix <wor@kth.se>
2019-03-09 15:30:39 +0000

    Changing the way the data updates are made.

--- InProgress-VueJS2/32-HandlingUserInputWithForms/src/App.vue
+++ InProgress-VueJS2/32-HandlingUserInputWithForms/src/App.vue
@@ -5,13 +5,17 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <h1>File a Complaint</h1>
                     <hr>
+                    <!-- v-model.lazy will update the values only when the field is left
+                         v-model.number will automatically convert the input into a number.
+                         The operations can be chained.
+                    -->
                     <div class="form-group">
                         <label for="email">Mail</label>
                         <input
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="userData.email">
+                                v-model.lazy="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
@@ -19,8 +23,7 @@
                                 type="password"
                                 id="password"
                                 class="form-control"
-                                v-model="userData.password"
-                                >
+                                v-model="userData.password">
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
@@ -28,7 +31,7 @@
                                 type="number"
                                 id="age"
                                 class="form-control"
-                                v-model="userData.age"
+                                v-model.number="userData.age"
                                 >
                     </div>
 
