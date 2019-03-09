bdb4aad207d93083add04607d7187fb0bdd78737
arcyfelix <wor@kth.se>
2019-03-09 16:36:42 +0000

    Using checkboxes.

--- InProgress-VueJS2/32-HandlingUserInputWithForms/src/App.vue
+++ InProgress-VueJS2/32-HandlingUserInputWithForms/src/App.vue
@@ -56,13 +56,17 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"
+                                    > Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"
+                                    > Send Infomail
                         </label>
                     </div>
 
@@ -118,7 +122,7 @@
                         <p style="white-space: pre">Message: {{ message }} </p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -139,8 +143,8 @@
             password: '',
             age: ''
           },
-          message: ''
-
+          message: '',
+          sendMail:[]
         }
       }
     }
