017b6d5e699c10de497eb5527c03f5d98a12c751
arcyfelix <wor@kth.se>
2019-03-13 21:50:30 +0000

    Local filter.

--- InProgress-VueJS2/34-Filters/src/App.vue
+++ InProgress-VueJS2/34-Filters/src/App.vue
@@ -3,7 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <!-- Adding a filter using | -->
+                <p>{{ text | toUppercase }}</p>
             </div>
         </div>
     </div>
@@ -11,6 +12,17 @@
 
 <script>
     export default {
+      data() {
+        return {
+          text: 'Hello there'
+        }
+      },
+      // Creating filter locally
+      filters: {
+        toUppercase(value) {
+          return value.toUpperCase();
+        }
+      }
     }
 </script>
 
