5b98fb53fe693ef6d9ed9bb0caa58305161a157b
arcyfelix <wor@kth.se>
2019-03-25 19:45:15 +0000

    Interceptors.

--- InProgress-VueJS2/37-Vue-Resource/src/main.js
+++ InProgress-VueJS2/37-Vue-Resource/src/main.js
@@ -7,6 +7,19 @@ Vue.use(VueResource);
 // Configuring Vue-resource link globally
 Vue.http.options.root = 'https://myvueproject-8a8df.firebaseio.com/myDataPackage.json';
 
+// Intercepting requests
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  // Changing the request type just for sake of changing it
+  if (request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    console.log('Response:');
+    console.log(response);
+  });
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
