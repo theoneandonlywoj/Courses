05098c624f932dccddc77f5acb78d39f25a7bc37
arcyfelix <wor@kth.se>
2019-03-26 21:08:29 +0000

    Navigating from code - imperative navigation.

--- InProgress-VueJS2/38-RoutingInVueJSApplication/src/components/user/User.vue
+++ InProgress-VueJS2/38-RoutingInVueJSApplication/src/components/user/User.vue
@@ -1,4 +1,18 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button v-on:click="navigateToHome" class="btn btn-primary">Go To Home</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      navigateToHome() {
+        this.$router.push({path: '/'});
+      }
+    }
+  }
+
+</script>
