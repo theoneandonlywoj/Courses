c2fec466b0410020f78413a8485ef2a9b89031e0
arcyfelix <wor@kth.se>
2019-04-04 21:12:13 +0100

    Loading routes lazily.

--- InProgress-VueJS2/38-RoutingInVueJSApplication/src/routes.js
+++ InProgress-VueJS2/38-RoutingInVueJSApplication/src/routes.js
@@ -1,9 +1,29 @@
-import User from './components/user/User.vue'
 import Home from './components/Home.vue'
-import UserStart from './components/user/UserStart.vue'
-import UserDetail from './components/user/UserDetail.vue'
-import UserEdit from './components/user/UserEdit.vue'
 import Header from './components/Header.vue'
+
+// Loading Routes Lazily
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'));
+  });
+}
+const UserStart = resolve => {
+  require.ensure(['./components/user/UserStart.vue'], () => {
+    resolve(require('./components/user/UserStart.vue'));
+  });
+}
+const UserDetail = resolve => {
+  require.ensure(['./components/user/UserDetail.vue'], () => {
+    resolve(require('./components/user/UserDetail.vue'));
+  });
+}
+const UserEdit = resolve => {
+  require.ensure(['./components/user/UserEdit.vue'], () => {
+    resolve(require('./components/user/UserEdit.vue'));
+  });
+}
+
+
 export const routes = [{
     name: "home",
     path: '',
