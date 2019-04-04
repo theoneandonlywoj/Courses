9d72a3fe3ada7d194c42012191408addd566e759
arcyfelix <wor@kth.se>
2019-04-04 08:56:51 +0100

    Adding scroll behavior using the hashes.

--- InProgress-VueJS2/38-RoutingInVueJSApplication/src/main.js
+++ InProgress-VueJS2/38-RoutingInVueJSApplication/src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
-import { routes } from './routes.js';
+import {
+  routes
+} from './routes.js';
 
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes: routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      if (to.hash) {
+        return {
+          selector: to.hash
+        };
+      } else {
+        return {
+          x: 0,
+          y: 0
+        };
+      }
+    }
+  }
 });
 
 new Vue({
