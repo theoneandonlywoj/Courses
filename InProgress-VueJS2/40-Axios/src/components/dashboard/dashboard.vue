d4cb2faef9a2f42000f64218c0610db47c339750
arcyfelix <wor@kth.se>
2019-04-12 22:15:44 +0100

    Getting the data back.

--- InProgress-VueJS2/40-Axios/src/components/dashboard/dashboard.vue
+++ InProgress-VueJS2/40-Axios/src/components/dashboard/dashboard.vue
@@ -2,8 +2,37 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your mail address: {{ email }}</p>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    data(){
+      return {
+        email: ''
+      }
+    },
+    created(){
+      axios.get('https://axios-9dd80.firebaseio.com/users.json')
+        .then(res => {
+          console.log(res);
+          const data = res.data;
+          const users = [];
+          for (let key in data){
+            const user = data[key]
+            user.id = key
+            users.push(user)
+          }
+          console.log(users);
+          this.email = users[0].email;
+        })
+        .catch(err => console.log(err))
+    }
+  }
+
+</script>
 
 <style scoped>
   h1, p {
