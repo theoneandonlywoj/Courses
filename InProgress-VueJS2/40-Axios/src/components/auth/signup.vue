f61882cbe7350b0e7607f318b14e2ce55ac881d0
arcyfelix <wor@kth.se>
2019-04-12 21:37:34 +0100

    Sending a POST request.

--- InProgress-VueJS2/40-Axios/src/components/auth/signup.vue
+++ InProgress-VueJS2/40-Axios/src/components/auth/signup.vue
@@ -69,6 +69,7 @@
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
     data () {
       return {
@@ -103,6 +104,9 @@
           terms: this.terms
         }
         console.log(formData)
+        axios.post('https://axios-9dd80.firebaseio.com/users.json', formData)
+          .then(res => console.log(res))
+          .catch(error => console.log(error))
       }
     }
   }
