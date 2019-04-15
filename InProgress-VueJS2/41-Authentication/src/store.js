265fa4fdc9f9458721d53919ebbd7c1e637f45be
arcyfelix <wor@kth.se>
2019-04-15 23:31:59 +0100

    Sending the token to the backend. Saving the token needs fixing.

--- InProgress-VueJS2/41-Authentication/src/store.js
+++ InProgress-VueJS2/41-Authentication/src/store.js
@@ -1,22 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from './axios-auth';
+import axios from './axios-auth'
+import globalAxios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     idToken: null,
-    userId: null
+    userId: null,
+    user: null
   },
   mutations: {
     authUser(state, userData){
       state.idToken = userData.token;
       state.userId = userData.userId;
+    },
+    storeuser(state, user){
+      state.user = user;
     }
   },
   actions: {
-    signup({commit}, authData){
+    signup({commit, dispatch}, authData){
       const firebaseExpectedSignUpData = {
         email: authData.email,
         password: authData.password,
@@ -28,7 +33,9 @@ export default new Vuex.Store({
           commit('authUser', {
             token: res.data.idToken,
             userId: res.data.localId
-          })
+          });
+          console.log('Dispatching storeuser');
+          dispatch('storeUser', authData);
         })
         .catch(error => console.log(error))
     },
@@ -47,9 +54,36 @@ export default new Vuex.Store({
           })
         })
         .catch(error => console.log(error))
+    },
+    storeUser({commit, state}, userData){
+      // If the idToken is not null
+      if (!state.idToken){
+        return
+      }
+      globalAxios('/users.json' + '?auth=' + state.idToken, userData)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    },
+    fetchUser({commit}){
+      globalAxios.get('/users.json' + '?auth=' + state.idToken)
+        .then(res => {
+          console.log(res)
+          const data = res.data
+          const users = []
+          for (let key in data) {
+            const user = data[key]
+            user.id = key
+            users.push(user)
+          }
+          console.log(users)
+          commit('storeUser', users[0])
+        })
+        .catch(error => console.log(error))
     }
   },
   getters: {
-
+    user(state){
+      return state.user
+    }
   }
 })
