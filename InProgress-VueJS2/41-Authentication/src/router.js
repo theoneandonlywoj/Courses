76a105b1315341f410ab105d08dd5df74f61890c
arcyfelix <wor@kth.se>
2019-04-16 22:24:35 +0100

    Adding a route guard for /dashboard.

--- InProgress-VueJS2/41-Authentication/src/router.js
+++ InProgress-VueJS2/41-Authentication/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from './store'
 
 import WelcomePage from './components/welcome/welcome.vue'
 import DashboardPage from './components/dashboard/dashboard.vue'
@@ -12,7 +13,19 @@ const routes = [
   { path: '/', component: WelcomePage },
   { path: '/signup', component: SignupPage },
   { path: '/signin', component: SigninPage },
-  { path: '/dashboard', component: DashboardPage }
+  { path: '/dashboard',
+    component: DashboardPage,
+    beforeEnter(to, from, next){
+      // Checking if the idToken is set
+      if (store.state.idToken){
+        // Continue...
+        next()
+      } else{
+        // Redirect to '/signin'
+        next('/signin')
+      }
+    }
+  }
 ]
 
 export default new VueRouter({mode: 'history', routes})
