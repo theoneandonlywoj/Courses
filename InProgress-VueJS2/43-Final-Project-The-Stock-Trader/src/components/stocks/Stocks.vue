90f4da1ecaba3320596dc64db31d05a9f8474459
arcyfelix <wor@kth.se>
2019-05-04 22:41:11 +0100

    Creating stocks components.

--- InProgress-VueJS2/43-Final-Project-The-Stock-Trader/src/components/stocks/Stocks.vue
+++ InProgress-VueJS2/43-Final-Project-The-Stock-Trader/src/components/stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div class="">
+    <stocksStock v-for="stock in stocks"></stocksStock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BWM', price: 110 },
+          { id: 2, name: 'AMZ', price: 100 },
+          { id: 3, name: 'TSLA', price: 90 }
+        ]
+      }
+    },
+    components: {
+      stocksStock: Stock
+    }
+  }
+
+</script>
