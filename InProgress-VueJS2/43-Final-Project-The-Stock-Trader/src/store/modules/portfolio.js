6f50b794c97c38011008ffe10f4a3696d392869f
arcyfelix <wor@kth.se>
2019-05-05 12:12:23 +0100

    Adding portfolio module to Vuex.

---
+++ InProgress-VueJS2/43-Final-Project-The-Stock-Trader/src/store/modules/portfolio.js
@@ -0,0 +1,63 @@
+const state = {
+  funds: 10000,
+  stocks: []
+};
+
+const mutations = {
+  'BUY_STOCK'(state, {stockId, quantity, stockPrice}){
+    // Find if I already own this specific stock
+    const record = state.stocks.find(element => return element.id == stockId)
+    if (record){
+      record.quantity += quantity;
+    } else {
+      state.stocks.push({
+        id: stockId,
+        quantity: quantity
+      })
+    }
+    state.funds -= stockPrice * quantity;
+  },
+  'SELL_STOCK'(state, {stockId, quantity, stockPrice}){
+    // Find if I already own this specific stock
+    const record = state.stocks.find(element => return element.id == stockId)
+    if (record.quantity > quantity){
+      record.quantity -= quantity;
+    } else {
+      state.stocks.splice(state.stocks.indexOf(records), 1);
+    }
+
+    state.funds += stockPrice * quantity;
+  }
+};
+
+const actions = {
+  // buyStock action is in the stocks.js file
+  sellStock({ commit }, payload){
+    // Payload = order
+    commit('SELL_STOCK', payload);
+  }
+};
+
+const getters = {
+  stockPortfolio(state, getters){
+    return state.stocks.map(stock => {
+      const record = getters.stocks.find(element => return element.id == stock.id);
+      return {
+        id: stock.id,
+        quantity: stock.quantity,
+        name: record.name,
+        price: record.price
+      }
+    })
+  },
+  funds(state){
+    return state.funds;
+  }
+}
+
+export default {
+  state: state,
+  mutations: mutations,
+  actions: actions,
+  getters: getters
+}
