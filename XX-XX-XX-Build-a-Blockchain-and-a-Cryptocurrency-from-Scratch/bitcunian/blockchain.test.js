8e2d611784cc58845820f2e70629398436c77b0d
theoneandonlywoj <wor@kth.se>
2020-01-26 19:09:24 +0000

    Adding more block to the valid chain and test for an invalid chain.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain.test.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain.test.js
@@ -21,8 +21,17 @@ describe('Blockchain', () => {
     });
 
     it('validates a valid chain', () => {
+        // Adding a block
         bc2.addBlock('data here');
+        bc2.addBlock('and one more block');
 
         expect(bc.isChainValid(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with corrupted data', () => {
+        // Corrupting the data of the genesis block
+        bc2.chain[0].data = 'Bad data';
+
+        expect(bc.isChainValid(bc2.chain)).toBe(false);
     });
 });
