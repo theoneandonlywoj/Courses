a9d00fc72ca663dbb16cf8e74d9c9a71f9725080
theoneandonlywoj <wor@kth.se>
2020-01-26 18:59:32 +0000

    Adding blockHash function and chain validation.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain.js
@@ -12,6 +12,25 @@ class Blockchain {
 
         return block;
     }
+
+
+    isChainValid(chain) {
+        // Checking if the genesis blocks are the same
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        } 
+
+        for(let i=1; i < chain.length; i++) {
+            const block = chain[i];
+            const previousBlock = chain[i - 1]
+
+            if(block.lastHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 module.exports = Blockchain;
