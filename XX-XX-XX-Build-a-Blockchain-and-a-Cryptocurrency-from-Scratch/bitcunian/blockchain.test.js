bc571cff6b2d4d248949b8a35049fc51dce532dc
theoneandonlywoj <wor@kth.se>
2020-01-26 19:06:44 +0000

    AddAdding a test to validate a valid chain.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain.test.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain.test.js
@@ -2,10 +2,11 @@ const Block = require('./block');
 const Blockchain = require('./blockchain');
 
 describe('Blockchain', () => {
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('starts with a genesis block', () => {
@@ -17,5 +18,11 @@ describe('Blockchain', () => {
         bc.addBlock(data);
 
         expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+    });
+
+    it('validates a valid chain', () => {
+        bc2.addBlock('data here');
+
+        expect(bc.isChainValid(bc2.chain)).toBe(true);
     });
 });
