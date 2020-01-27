3da020d0a05f20acae5055fd8e3c8d0636664fcc
theoneandonlywoj <wor@kth.se>
2020-01-27 20:41:30 +0000

    Adding test for the updated Block to ensure the hashing with Nonce works correctly.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain/block.test.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain/block.test.js
@@ -1,5 +1,7 @@
 const Block = require('./block');
 
+const { DIFFICULTY } = require('../config');
+
 describe('Block', () => {
     let data, lastBlock, block;
 
@@ -15,5 +17,9 @@ describe('Block', () => {
 
     it('sets the `lastHash` to match hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('generates a hash with number of the leading zero equal to the difficulty', () => {
+        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
     });
 });
