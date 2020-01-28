17c37c4e0d4f1c8ab2c4173e3d0b74ae56dedb9a
theoneandonlywoj <wor@kth.se>
2020-01-28 21:11:50 +0000

    Adding a Transaction class.

---
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/wallet/transaction.js
@@ -0,0 +1,31 @@
+const ChainUtil = require('../chain-util');
+
+class Transaction {
+    constructor() {
+        this.id = ChainUtil.id();
+        this.input = null;
+        this.outputs = [];
+    }
+
+    static newTransaction(senderWallet, recipient, amount) {
+        const transaction = new this();
+        if(amount > senderWallet.balance) {
+            console.log(`Amount ${amount} exceeds the balance`);
+            return;
+        }
+
+        // First output is the sender and his / her new balance
+        // Second is the recipient and his / her additional balance
+        // ... is ES6 decomposition functionality
+        transaction.outputs.push(...[
+            { amount: senderWallet - amount,
+              address: senderWallet.publicKey },
+            { amount: amount, 
+              address: recipient.publicKey}
+        ])
+
+        return transaction;
+    }
+}
+
+module.exports = Transaction;
