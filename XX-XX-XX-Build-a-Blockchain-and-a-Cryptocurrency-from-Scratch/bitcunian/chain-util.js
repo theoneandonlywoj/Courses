1f01a061e026684432059701a778ac36e638bc07
theoneandonlywoj <wor@kth.se>
2020-01-28 20:52:01 +0000

    Adding notes for installing Elliptic and UUID and adding new stuff to chain-utils.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/chain-util.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/chain-util.js
@@ -1,4 +1,6 @@
 const EC = require('elliptic').ec;
+// Timestamp based UUID
+const uuidV1 = require('uuid/v1');
 
 // https://en.bitcoin.it/wiki/Secp256k1
 const ec = new EC('secp256k1');
@@ -6,6 +8,10 @@ const ec = new EC('secp256k1');
 class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
+    }
+
+    static id() {
+        return uuidV1();
     }
 }
 
--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/notes.md
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/notes.md
@@ -48,7 +48,12 @@ HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
 HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001, ws://localhost:5002 npm run dev
 ```
 
-## Installing Elliptic
+## Install Elliptic
 ```bash
 npm i elliptic --save
+```
+
+## Install UUID
+```bash
+npm i uuid --save
 ```
