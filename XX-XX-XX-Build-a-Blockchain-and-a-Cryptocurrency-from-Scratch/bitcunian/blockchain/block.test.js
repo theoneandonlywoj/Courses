3abd76c256c68f53b61cafc6efcd14bd1fde6378
theoneandonlywoj <wor@kth.se>
2020-01-28 16:04:45 +0000

    Adding dynamic mining difficulty and tests for it.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain/block.test.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/blockchain/block.test.js
@@ -1,6 +1,5 @@
 const Block = require('./block');
-
-const { DIFFICULTY } = require('../config');
+const { MINE_RATE } = require('../config');
 
 describe('Block', () => {
     let data, lastBlock, block;
@@ -20,6 +19,16 @@ describe('Block', () => {
     });
 
     it('generates a hash with number of the leading zero equal to the difficulty', () => {
-        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    it('lowers difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + (MINE_RATE * 2)))
+            .toEqual(block.difficulty - 1)
+    });
+
+    it('increases difficulty for rapidly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, (block.timestamp + (MINE_RATE / 4))))
+            .toEqual(block.difficulty + 1)
     });
 });
--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/config.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/config.js
@@ -1,3 +1,4 @@
-const DIFFICULTY = 4;
+const DIFFICULTY = 2;
+const MINE_RATE = 10000; // 1000 = 1s for a block 
 
-module.exports = { DIFFICULTY };
+module.exports = { DIFFICULTY, MINE_RATE };
