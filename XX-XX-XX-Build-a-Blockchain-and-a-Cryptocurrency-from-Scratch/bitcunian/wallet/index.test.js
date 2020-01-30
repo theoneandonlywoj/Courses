14edde788b91b1cb0f7c6b4800c58399e87ce794
theoneandonlywoj <wor@kth.se>
2020-01-30 15:29:14 +0000

    Adding wallet tests.

---
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/wallet/index.test.js
@@ -0,0 +1,41 @@
+const Wallet = require('./index');
+const TransactionPool = require('./transaction-pool');
+
+describe('Wallet', () => {
+    let wallet, tp;
+
+    beforeEach(() => {
+        wallet = new Wallet();
+        tp = new TransactionPool();
+    });
+
+    describe('creating a transaction', () => {
+        let transaction, sendAmount, recipient;
+
+        beforeEach(() => {
+            sendAmount = 50;
+            recipient = 'r4nd0m';
+            transaction = wallet.createTransaction(recipient, sendAmount, tp);
+        });
+
+        describe('and doing the same transaction', () => {
+            beforeEach(() => {
+                // Repeating the same transaction as above.
+                wallet.createTransaction(recipient, sendAmount, tp);
+            });
+
+            it('doubles the `sendAmount` subtracked from the wallet balance', () => {
+                const transactionForSenderId =
+                    transaction.outputs.find(output => output.address === wallet.publicKey);
+                expect(transactionForSenderId.amount).toEqual(wallet.balance - sendAmount * 2);
+            });
+
+            it('clones the `sendAmount` for the recipient', () => {
+                const transactionsForRecipient = 
+                    transaction.outputs.filter(output => output.address === recipient);
+                const transactionAmounts = transactionsForRecipient.map(t => t.amount);
+                expect(transactionAmounts).toEqual([sendAmount, sendAmount]);
+            });
+        });
+    });
+});
