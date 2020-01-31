7b42105cf8ba4a5b1edcbb6630c3ad2b3b23074f
theoneandonlywoj <wor@kth.se>
2020-01-31 13:33:59 +0000

    Adding tests to ensure only valid transactions are passed throught.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/wallet/transaction-pool.test.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/wallet/transaction-pool.test.js
@@ -26,4 +26,29 @@ describe('TransactionPool', () => {
 
         expect(JSON.stringify(transactionForTransactionId)).not.toEqual(oldTransaction);
     });
+
+    describe('mixing valid and corrupt transactions', () => {
+        let validTransactions;
+
+        beforeEach(() => {
+          validTransactions = [...tp.transactions];
+          for (let i=0; i<6; i++) {
+            wallet = new Wallet();
+            transaction = wallet.createTransaction('r4nd-4dr355', 30, tp);
+            if (i%2==0) {
+              transaction.input.amount = 9999;
+            } else {
+              validTransactions.push(transaction);
+            }
+          }
+        });
+      
+        it('shows a difference between valid and corrupt transactions', () => {
+          expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
+        });
+      
+        it('grabs valid transactions', () => {
+          expect(tp.validTransactions()).toEqual(validTransactions);
+        });
+      });
 });
