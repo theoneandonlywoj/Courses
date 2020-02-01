5ea2bc79762953df8000e8dc85ca3a3df6df5fda
theoneandonlywoj <wor@kth.se>
2020-02-01 20:53:37 +0000

    Refactoring newTransaction method.

--- XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/wallet/transaction.js
+++ XX-XX-XX-Build-a-Blockchain-and-a-Cryptocurrency-from-Scratch/bitcunian/wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util');
+const { MINING_REWARD } = require('../config');
 
 class Transaction {
     constructor() {
@@ -22,24 +23,26 @@ class Transaction {
         return this;
     }
 
-    static newTransaction(senderWallet, recipient, amount) {
+    static transactionWithOutputs(senderWallet, outputs) {
         const transaction = new this();
+        transaction.outputs.push(...outputs);
+        Transaction.signTransaction(transaction, senderWallet);
+        return transaction;
+    }
+
+    static newTransaction(senderWallet, recipient, amount) {
         if(amount > senderWallet.balance) {
             console.log(`Amount ${amount} exceeds the balance`);
             return;
         }
 
-        // First output is the sender and his / her new balance
-        // Second is the recipient and his / her additional balance
-        // ... is ES6 decomposition functionality
-        transaction.outputs.push(...[
+        const transaction = Transaction.transactionWithOutputs(senderWallet, [
             { amount: senderWallet.balance - amount,
               address: senderWallet.publicKey },
             { amount: amount, 
               address: recipient}
-        ])
+        ]);
 
-        Transaction.signTransaction(transaction, senderWallet);
         return transaction;
     }
 
