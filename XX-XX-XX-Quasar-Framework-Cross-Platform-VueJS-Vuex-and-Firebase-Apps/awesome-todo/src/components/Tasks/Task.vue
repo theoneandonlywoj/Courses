6957bb5cce29a8ab00537b5d4328397a156d8e4d
theoneandonlywoj <wor@kth.se>
2020-03-02 14:57:51 +0000

    Putting task into a child component.

---
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/components/Tasks/Task.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-item
+    @click="task.completed = !task.completed"
+    clickable
+    v-ripple
+    :class="!task.completed ? 'bg-orange-1' : 'bg-green-1' ">
+    <q-item-section side top>
+        <q-checkbox v-model="task.completed" />
+    </q-item-section>
+
+    <q-item-section>
+        <q-item-label
+        :class="{
+            'text-strike': task.completed
+        }">
+        {{ task.name }}
+        </q-item-label>
+    </q-item-section>
+
+        <q-item-section side>
+        <div class="row">
+        <div class="column justify-center">
+            <q-icon
+            name="event"
+            size="18px"
+            class="q-mr-xs"
+            ></q-icon>
+        </div>
+        <div class="column">
+            <q-item-label
+            caption
+            class="row justify-end"
+            >
+            {{ task.dueDate}}
+            </q-item-label>
+            <q-item-label
+            caption
+            class="row justify-end"
+            >
+            <small>{{ task.dueTime }}</small>
+            </q-item-label>
+        </div>
+        </div>
+    </q-item-section>
+
+    </q-item>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: ['task', 'taskId']
+}
+</script>
+
+<style>
+
+</style>
--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageTodo.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageTodo.vue
@@ -3,61 +3,25 @@
     <q-list
       separator
       bordered>
-      <q-item
+
+      <Task
         v-for="(task, key) in tasks"
         :key="key"
-        @click="task.completed = !task.completed"
-        clickable
-        v-ripple
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1' ">
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label
-            :class="{
-              'text-strike': task.completed
-            }">
-            {{ task.name }}
-          </q-item-label>
-        </q-item-section>
-
-         <q-item-section side>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon
-                name="event"
-                size="18px"
-                class="q-mr-xs"
-              ></q-icon>
-            </div>
-            <div class="column">
-              <q-item-label
-                caption
-                class="row justify-end"
-              >
-                {{ task.dueDate}}
-              </q-item-label>
-              <q-item-label
-                caption
-                class="row justify-end"
-              >
-                <small>{{ task.dueTime }}</small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-
-      </q-item>
+        :task="task">
+      </Task>
 
     </q-list>
   </q-page>
 </template>
 
 <script>
+import Task from '../components/Tasks/Task'
+
 export default {
   name: 'PageTodo',
+  components: {
+    Task
+  },
   computed: {
     tasks () {
       return this.$store.getters['tasks/tasksGetter']
