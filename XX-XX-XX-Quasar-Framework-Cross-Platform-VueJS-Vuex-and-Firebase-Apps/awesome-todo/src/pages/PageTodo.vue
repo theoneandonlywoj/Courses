06f613b6bdf4d0862f05f1eb9210a418e16dc1e9
theoneandonlywoj <wor@kth.se>
2020-03-02 08:21:55 +0000

    Adding interactive list of tasks.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageTodo.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageTodo.vue
@@ -1,11 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
-    <p>Todo Page</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageTodo'
+  name: 'PageTodo',
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go shopping',
+          completed: false
+        },
+        {
+          id: 2,
+          name: 'Work',
+          completed: false
+        },
+        {
+          id: 3,
+          name: 'Get bananas',
+          completed: false
+        }
+      ]
+    }
+  }
 }
 </script>
