d856b644abaa66045558ef88f4561db3aac7dba1
theoneandonlywoj <wor@kth.se>
2020-03-02 14:06:55 +0000

    Adapting the store to the Firebase object setup.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageTodo.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageTodo.vue
@@ -4,8 +4,8 @@
       separator
       bordered>
       <q-item
-        v-for="task in tasks"
-        :key="task.id"
+        v-for="(task, key) in tasks"
+        :key="key"
         @click="task.completed = !task.completed"
         clickable
         v-ripple
