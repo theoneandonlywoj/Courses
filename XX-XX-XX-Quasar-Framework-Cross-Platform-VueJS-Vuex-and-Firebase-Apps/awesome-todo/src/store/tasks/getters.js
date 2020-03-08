a0e0806561245aa742b8a0777271949933fabea0
theoneandonlywoj <wor@kth.se>
2020-03-08 11:01:05 +0000

    Adding getters for tasks to be done and completed tasks.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/store/tasks/getters.js
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/store/tasks/getters.js
@@ -1,3 +1,24 @@
 export function tasksGetter (state) {
   return state.tasks
 }
+export function tasksToDoGetter (state) {
+  const tasksToDo = {}
+  Object.keys(state.tasks).forEach(key => {
+    const task = state.tasks[key]
+    if (!task.completed) {
+      tasksToDo[key] = task
+    }
+  })
+  return tasksToDo
+}
+
+export function tasksCompletedGetter (state) {
+  const tasksCompleted = {}
+  Object.keys(state.tasks).forEach(key => {
+    const task = state.tasks[key]
+    if (task.completed) {
+      tasksCompleted[key] = task
+    }
+  })
+  return tasksCompleted
+}
