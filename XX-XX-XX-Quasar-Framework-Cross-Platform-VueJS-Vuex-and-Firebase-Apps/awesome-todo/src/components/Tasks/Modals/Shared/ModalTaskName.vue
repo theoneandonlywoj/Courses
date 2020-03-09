99cba196ba85a7129b45c3c9ff2a0e6ee130f917
theoneandonlywoj <wor@kth.se>
2020-03-09 22:21:19 +0000

    Adding a directive so that if the field is focused on, it will select the whole text.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/components/Tasks/Modals/Shared/ModalTaskName.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/components/Tasks/Modals/Shared/ModalTaskName.vue
@@ -11,13 +11,27 @@
             ref="newTaskName"
             :rules="[val => !!val || 'Field is required']"
             @input="$emit('update:taskName', $event)"
+            v-select-all
         />
     </div>
 </template>
 
 <script>
 export default {
-  props: ['taskName']
+  props: ['taskName'],
+  directives: {
+    selectAll: {
+      inserted (el) {
+        // When the element is insterted and the length of the value > 0, select all.
+        const input = el.querySelector('.q-field__native')
+        input.addEventListener('focus', () => {
+          if (input.value.length) {
+            input.select()
+          }
+        })
+      }
+    }
+  }
 }
 </script>
 
