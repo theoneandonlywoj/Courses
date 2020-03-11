2858be02a3ca83e7d818d7f92e24912a4d59503e
theoneandonlywoj <wor@kth.se>
2020-03-11 09:02:33 +0000

    Adding Email Us.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/Settings.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/Settings.vue
@@ -1,17 +1,28 @@
 <template>
   <q-page>
     <q-list>
-    <q-item-label header>Notifications</q-item-label>
-      <q-item tag="label" v-ripple>
-        <q-item-section>
-          <q-item-label>Some Local Storage Setting</q-item-label>
-        </q-item-section>
-        <q-item-section side >
-          <q-toggle color="blue"
-            v-model="someLocalStorageSetting"
-          />
-        </q-item-section>
-      </q-item>
+      <q-item-label header>Settings</q-item-label>
+        <q-item tag="label" v-ripple>
+          <q-item-section>
+            <q-item-label>Some Local Storage Setting</q-item-label>
+          </q-item-section>
+          <q-item-section side >
+            <q-toggle color="blue"
+              v-model="someLocalStorageSetting"
+            />
+          </q-item-section>
+        </q-item>
+    </q-list>
+    <q-list>
+      <q-item-label header>More</q-item-label>
+        <q-item
+          tag="label"
+          v-ripple
+          @click="emailUs()">
+          <q-item-section>
+            <q-item-label>Email Us</q-item-label>
+          </q-item-section>
+        </q-item>
     </q-list>
   </q-page>
 </template>
@@ -27,6 +38,11 @@ export default {
       set (value) {
         this.$store.dispatch('settings/setSomeLocalStorageSettingAction', value)
       }
+    }
+  },
+  methods: {
+    emailUs () {
+      window.location.href = 'mailto:wor@kth.se?subject=Awesome To Do Feedback'
     }
   }
 }
