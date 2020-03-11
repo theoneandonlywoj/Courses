0a4e071f41d59d7ba9b24ee088a3dfd495f596fc
theoneandonlywoj <wor@kth.se>
2020-03-11 17:29:08 +0000

    Adding the Login and Register page in one component.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageAuth.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageAuth.vue
@@ -1,47 +1,52 @@
 <template>
   <q-page padding>
-    <q-card>
-        <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="login" label="Login" />
-          <q-tab name="register" label="Register" />
-        </q-tabs>
+    <q-card class="auth-tabs">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
 
-        <q-separator />
+      <q-separator />
 
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="login">
-            <div class="text-h6">Mails</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <LoginRegister :tab="tab" />
+        </q-tab-panel>
 
-          <q-tab-panel name="register">
-            <div class="text-h6">Alarms</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
-        </q-tab-panels>
-      </q-card>
+        <q-tab-panel name="register">
+          <LoginRegister :tab="tab" />
+        </q-tab-panel>
+
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
+import LoginRegister from './../components/Auth/LoginRegister'
 export default {
-  name: 'PageAuth',
   data () {
     return {
-      tab: 'login'
+      tab: 'register'
     }
+  },
+  components: {
+    LoginRegister
   }
 }
 </script>
 
 <style>
-
+  .auth-tabs {
+    max-width: 500px;
+    margin: 0 auto;
+  }
 </style>
