92d61e28a3edcc3f546c028e2e191cdfc46238e5
theoneandonlywoj <wor@kth.se>
2020-03-11 16:22:12 +0000

    Adding login and register panels.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageAuth.vue
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/pages/PageAuth.vue
@@ -1,12 +1,44 @@
 <template>
   <q-page padding>
-    <p>Page Auth</p>
+    <q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="register">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  name: 'PageAuth',
+  data () {
+    return {
+      tab: 'login'
+    }
+  }
 }
 </script>
 
