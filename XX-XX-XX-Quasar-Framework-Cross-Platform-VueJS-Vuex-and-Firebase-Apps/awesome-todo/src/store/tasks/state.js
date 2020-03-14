38b7efa46202ba9c95e84e148cdd7062683b881e
theoneandonlywoj <wor@kth.se>
2020-03-14 21:25:14 +0000

    Deleting obsolete test tasks.

--- XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/store/tasks/state.js
+++ XX-XX-XX-Quasar-Framework-Cross-Platform-VueJS-Vuex-and-Firebase-Apps/awesome-todo/src/store/tasks/state.js
@@ -2,19 +2,6 @@ export default function () {
   return {
     search: '',
     sortBy: 'name',
-    tasks: {
-      ID1: {
-        name: 'one',
-        dueDate: '01/01/2020',
-        dueTime: '18:00',
-        completed: false
-      },
-      ID2: {
-        name: 'two',
-        dueDate: '01/01/2020',
-        dueTime: '18:00',
-        completed: false
-      }
-    }
+    tasks: {}
   }
 }
