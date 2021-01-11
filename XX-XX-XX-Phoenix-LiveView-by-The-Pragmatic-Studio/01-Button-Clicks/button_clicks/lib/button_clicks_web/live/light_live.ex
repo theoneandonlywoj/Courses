2057346521332869ba65d5deeed9f6f7f121951f
theoneandonlywoj <theoneandonlywoj@gmail.com>
2021-01-11 23:50:59 +0100

    Adding buttons.

--- XX-XX-XX-Phoenix-LiveView-by-The-Pragmatic-Studio/01-Button-Clicks/button_clicks/lib/button_clicks_web/live/light_live.ex
+++ XX-XX-XX-Phoenix-LiveView-by-The-Pragmatic-Studio/01-Button-Clicks/button_clicks/lib/button_clicks_web/live/light_live.ex
@@ -13,6 +13,34 @@ defmodule ButtonClicksWeb.LightLive do
         <%= @brightness %>%
       </span>
       </div>
+      <button phx-click="off">
+        Off
+      </button>
+      <button phx-click="on">
+        On
+      </button>
+      <button phx-click="down">
+        Down
+      </button>
+      <button phx-click="up">
+        Up
+      </button>
     """
+  end
+
+  def handle_event("on", _value, socket) do
+    {:noreply, assign(socket, :brightness, 100)}
+  end
+
+  def handle_event("off", _value, socket) do
+    {:noreply, assign(socket, :brightness, 0)}
+  end
+
+  def handle_event("down", _value, socket) do
+    {:noreply, update(socket, :brightness, fn b -> max(b - 10, 0) end)}
+  end
+
+  def handle_event("up", _value, socket) do
+    {:noreply, update(socket, :brightness, fn b -> min(b + 10, 100) end)}
   end
 end
