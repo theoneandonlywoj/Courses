36452bd5cfa6be01f02557dfa6be264bccb78b29
theoneandonlywoj <theoneandonlywoj@gmail.com>
2022-10-22 18:21:13 +0200

    Places - migration.

--- XX-XX-XX-Full-Stack GraphQL with Absinthe Phoenix and React/getaways/priv/repo/migrations/20221022161418_create_places_table.exs
+++ XX-XX-XX-Full-Stack GraphQL with Absinthe Phoenix and React/getaways/priv/repo/migrations/20221022161418_create_places_table.exs
@@ -1,36 +1,3 @@
-# Full-Stack GraphQL with Absinthe Phoenix and React
-https://pragmaticstudio.com/courses/unpacked-full-stack-graphql-with-absinthe-phoenix-react
-
-# Steps
-## Create a new project
-```sh
-mix phx.new getaways --module Getaways
-```
-
-## Add the following libraries (mix.exs)
-```elixir
-...
-{:absinthe, "~> 1.6.1"},
-{:absinthe_plug, "~> 1.5.8"},
-{:absinthe_phoenix, "~> 2.0.1"},
-{:pbkdf2_elixir, "~> 1.0"},
-{:cors_plug, "~> 2.0"},
-{:dataloader, "~> 1.0.6"}
-...
-```
-
-## Change the settings for the dev password if needed and create the database.
-```sh
-mix ecto.create
-```
-
-## Create the first migration
-```sh
-mix ecto.gen.migration create_places_table
-```
-
-## Content of the migration (priv/repo/migrations/20221022161418_create_places_table.exs)
-```elixir
 defmodule Getaways.Repo.Migrations.CreatePlacesTable do
   use Ecto.Migration
 
@@ -55,5 +22,3 @@ defmodule Getaways.Repo.Migrations.CreatePlacesTable do
     create unique_index(:places, [:slug])
   end
 end
-
-```
